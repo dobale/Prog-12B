fe98df8d3b74e7f301e045803ffb0e48ec3c6fcc
dobale <johncoofe94@gmail.com>
2026-09-17 08:42:28 +0200

    Update github_teszt.py
    Did a yea

--- github_teszt.py
+++ github_teszt.py
@@ -12,3 +12,13 @@ for szam in szamok:
 atlag = atlag / len(szamok)
 
 print(f"a számok átlaga {atlag}")
+
+# Van-e páros szám?
+i = 0
+while i < len(szamok) and szamok[i] % 2 != 0:
+    i += 1
+
+if i < len(szamok):
+    print("Van páros szám")
+else:
+    print("Nincs páros szám")
